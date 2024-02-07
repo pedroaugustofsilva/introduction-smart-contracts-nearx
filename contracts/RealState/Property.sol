e32620d0d6a5ae108be7bc0205baeb680032377e
Pedro Augusto <pedroaugusto@hey.com>
2024-02-07 13:07:32 -0300

    the client must be approved to purchase the property

--- contracts/RealState/Property.sol
+++ contracts/RealState/Property.sol
@@ -5,25 +5,40 @@ pragma solidity ^0.8.20;
 contract Property {
     address public realState;
     address public broker;
-    address public owner;
+    address public currentOwner;
 
     uint256 public price;
     uint256 public comission;
     uint256 public timeToPay;
 
+    mapping(address client => bool approved) public approvedClients;
+
     constructor(
         address _realState,
         address _broker,
-        address _owner,
+        address _currentOwner,
         uint256 _price,
         uint256 _comission,
         uint256 _timeToPay
     ) {
         realState = _realState;
         broker = _broker;
-        owner = _owner;
+        currentOwner = _currentOwner;
         price = _price;
         comission = _comission;
         timeToPay = _timeToPay;
+    }
+
+    function approveSaleFor(address _client) onlyBroker external {
+        approvedClients[_client] = true;
+    }
+
+    function removeSalesApprovalFrom(address _client) onlyBroker external {
+        approvedClients[_client] = false;
+    }
+
+    modifier onlyBroker() {
+        require(msg.sender == broker, "Restricted to broker");
+        _;
     }
 }
