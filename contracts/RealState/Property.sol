179b7ba543d5e01e5c6a41b58677066b9080959a
Pedro Augusto <pedroaugusto@hey.com>
2024-02-09 06:53:11 -0300

    added withdraw to Property

--- contracts/RealState/Property.sol
+++ contracts/RealState/Property.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.20;
 contract Property {
     address public realState;
     address public broker;
-    address public currentOwner;
+    address private currentOwner;
 
     uint256 public price;
-    uint256 public commission;
-    uint256 public timeToPay;
+    uint256 private commission;
+    uint256 private timeDoubleCommission;
 
-    mapping(address buyer => bool approved) public _approvedBuyers;
-    mapping(address recipient => uint256 value) public _balances;
+    mapping(address buyer => bool approved) private _approvedBuyers;
+    mapping(address recipient => uint256 value) private _balances;
 
     constructor(
         address _realState,
@@ -20,14 +20,14 @@ contract Property {
         address _currentOwner,
         uint256 _price,
         uint256 _commission,
-        uint256 _timeToPay
+        uint256 _timeDoubleCommission
     ) {
         realState = _realState;
         broker = _broker;
         currentOwner = _currentOwner;
         price = _price;
         commission = _commission;
-        timeToPay = _timeToPay;
+        timeDoubleCommission = _timeDoubleCommission;
     }
 
     function purchase() external payable {
@@ -48,11 +48,20 @@ contract Property {
     function removeSalesApprovalFrom(address _buyer) onlyBroker external {
         _approvedBuyers[_buyer] = false;
     }
+    
+    function withdraw() external {
+        require(_balances[msg.sender] > 0, "There is no value to withdraw");
+
+        uint256 amount = _balances[msg.sender];
+        _balances[msg.sender] = 0;
+  
+        payable(msg.sender).transfer(amount);
+    }
 
     function brokerCommission() private view returns (uint256) {
         uint256 baseCommission = (price * commission) / 100;
 
-        if (timeToPay >= block.timestamp) {
+        if (timeDoubleCommission >= block.timestamp) {
             baseCommission = baseCommission * 2;                
         }
 
